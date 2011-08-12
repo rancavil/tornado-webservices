702196122a7fae627e28d935da759733df353c90
Rodrigo Ancavil del Pino <rancavil@innovaser.cl>
2011-08-12 00:01:21 -0400

    new web service example with the use of complextypes

---
+++ demos/ProductService.py
@@ -0,0 +1,51 @@
+import tornado.httpserver
+import tornado.ioloop
+from tornadows import soaphandler
+from tornadows import webservices
+from tornadows import xmltypes
+from tornadows import complextypes
+from tornadows.soaphandler import webservice
+
+class Input(complextypes.ComplexType):
+	idProduct = complextypes.IntegerProperty()
+
+class Product(complextypes.ComplexType):
+	id    = complextypes.IntegerProperty()
+	name  = complextypes.StringProperty()
+	price = complextypes.FloatProperty()
+	stock = complextypes.IntegerProperty()
+
+class ProductService(soaphandler.SoapHandler):
+  	@webservice(_params=Input,_returns=Product)
+  	def getProduct(self, input):
+     		id = input.idProduct.value
+		
+		reg = self.database(id)
+
+     		output = Product()
+		output.id.value    = id
+     		output.name.value  = reg[0]
+		output.price.value = reg[1]
+		output.stock.value = reg[2]
+
+     		return output
+
+	def database(self,id):
+	""" This method simulates a database of products """
+		db = {1:('COMPUTER',1000.5,100),
+ 		      2:('MOUSE',10.0,300),
+		      3:('PENCIL',0.50,500),
+		     }
+		row = (None,None,None)
+		try:
+			row = db[id]
+		except:
+			None
+		return row
+	
+if __name__ == '__main__':
+  service = [('ProductService',ProductService)]
+  app = webservices.WebService(service)
+  ws  = tornado.httpserver.HTTPServer(app)
+  ws.listen(8080)
+  tornado.ioloop.IOLoop.instance().start()
