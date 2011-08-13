d234678f1ff43b71f3384b38192a659f8803491d
Rodrigo Ancavil del Pino <rancavil@innovaser.cl>
2011-08-12 23:29:25 -0400

    fixed the problem of Indentation Error

--- demos/ProductService.py
+++ demos/ProductService.py
@@ -31,12 +31,15 @@ class ProductService(soaphandler.SoapHandler):
      		return output
 
 	def database(self,id):
-	""" This method simulates a database of products """
+		""" This method simulates a database of products """
 		db = {1:('COMPUTER',1000.5,100),
  		      2:('MOUSE',10.0,300),
-		      3:('PENCIL',0.50,500),
+		      3:('PENCIL BLUE',0.50,500),
+		      4:('PENCIL RED',0.50,600),
+		      5:('PENCIL WHITE',0.50,900),
+		      6:('HEADPHONES',15.7,500),
 		     }
-		row = (None,None,None)
+		row = (None,0.0,0)
 		try:
 			row = db[id]
 		except:
