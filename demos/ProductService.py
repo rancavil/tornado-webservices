48b1e58fee8c5133ff5d8833583aaa6f6a195387
Rodrigo Ancavil del Pino <rancavil@innovaser.cl>
2011-09-18 20:31:24 -0300

    file update and checked

--- demos/ProductService.py
+++ demos/ProductService.py
@@ -1,9 +1,43 @@
+#!/usr/bin/env python
+#
+# Copyright 2011 Rodrigo Ancavil del Pino
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
 import tornado.httpserver
 import tornado.ioloop
 from tornadows import soaphandler
 from tornadows import webservices
 from tornadows import complextypes
 from tornadows.soaphandler import webservice
+
+""" This web service implements two python classes as two xml complextypes.
+
+    Input is a class that represents the input parameter (request) for the
+    web service:
+
+	idProduct : is a instance of IntegerProperty(). This create a subclass
+		    of Property with value attribute (for store the value) and
+		    type attribute for store the xmltype.
+
+    Product is a class that represents the response of the web service.
+
+	id: Is a instance of IntegerProperty() that store the id of product
+	name: Is a instance of StringProperty() that store the name of product 
+	price: Is a instance of FloatProperty() that store the price of product
+	stock: Is a instance of IntegerProperty() that store the stock of product
+
+"""
 
 class Input(complextypes.ComplexType):
 	idProduct = complextypes.IntegerProperty()
@@ -22,6 +56,7 @@ class ProductService(soaphandler.SoapHandler):
 		reg = self.database(id)
 
 		output = Product()
+
 		output.id.value    = id
 		output.name.value  = reg[0]
 		output.price.value = reg[1]
