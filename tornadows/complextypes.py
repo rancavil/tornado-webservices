64bd80d50006f8b6d6605eaad1c92537342f19bf
Rodrigo Ancavil del Pino <rancavil@innovaser.cl>
2011-09-19 02:05:56 -0300

    improved the management of complex types and the use of python list as xml element

--- tornadows/complextypes.py
+++ tornadows/complextypes.py
@@ -241,7 +241,7 @@ class ComplexType(object):
 				continue
 			if isinstance(element,Property):
 				xsd += element.type.createElement(str(key))
-			elif isinstance(element,ComplexType) or issublclass(element,ComplexType):
+			elif isinstance(element,ComplexType) or issubclass(element,ComplexType):
 				nameinstance = key
 				complextype.append(element._generateXSD())
 				xsd += b'<%s:element name="%s" type="tns:%s"/>'%(namespace,nameinstance,element.getName())
